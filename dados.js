dfb32806ea352c00e50e507e715e1afee0426e7f
marceloandrade1993 <marcelowow13@hotmail.com>
2024-09-07 19:24:25 -0300

    reupload3

--- dados.js
+++ dados.js
@@ -1,32 +1,76 @@
 let dados = [ {
     
-    titulo : "treino Inferiores perna coxa",
-    descriçao : "Agachemtno: fazer 3 series de 15 reptiçoes",
-    videoUrl: "img/videoagachar.mp4",
+    titulo : "Inferior perna coxa",
+    descriçao : "quadriceps: fazer 3 series de 15 reptiçoes",
+    videoUrl: "img/videoperna.mp4",
     
     
 },
 
 {
-    titulo : "treino Superiores peito ombro",
-    descriçao : "flexao: fazer 3 séries de 15 repetições",
+    titulo : "superior tronco ",
+    descriçao : "peitoral : fazer 3 séries de 15 repetições",
     videoUrl : "img/videopeito.mp4"
    
 },
 
 {
-    titulo : "treino Costas lombar ",
+    titulo : "treino Costas",
     descriçao : "Costa : fazer 3 series de 30 seg",
     videoUrl : "img/videocosta.mp4"
 },
 
 {
-    titulo : "treino Braço biceps triceps antebraço",
+    titulo : "membro antebraço ante-braço",
     descriçao : "antebraço : fazer 3 series de 10 repetições",
     videoUrl : "img/videobraço.mp4"
     
+},
+{
+    titulo : " triceps",
+    descriçao : "triceps: fazer 3 series de 15 reptiçoes",
+    videoUrl : "imag/videobraço33.mp4"
+
+},
+{
+    titulo : "Biceps",
+    descriçao :"biceps : fazer 3 series de 15 repetiçoes",
+    videoUrl : "img/videobraço2.mp4"
+
+},
+{
+
+    titulo : "Dorsal",
+    descriçao : "Dorsal : fazer 3 series de 15 repetiçoes",
+    videoUrl : "img/videocosta1.mp4"
+},
+{
+
+    titulo : "lombar trapezio",
+    descriçao : "lombar trapesio : fazer 3 no max que der conta",
+    videoUrl : "img/videocosta2.mp4"
+},
+{
+    titulo : "posterior gluteo",
+    descriçao : "Glutéo panturrilha pé : fazer 3 series",
+    videoUrl : "img/videoperna1.mp4"
+
+
+},
+
+{
+    titulo : "peito",
+    descriçao : "Treino Peitoral : 3 series de 15 repetiçoes",
+    videoUrl : "img/videopeito2.mp4"
+},
+
+{
+titulo : "ombro deltoide",
+descriçao : "ombros : 3 series de 15 repetiçoes",
+videoUrl : "img/videopeito1.mp4"
+
 }
-       
+
 ];
 
 
