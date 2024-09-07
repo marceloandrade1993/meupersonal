58cba9073cf0685a82715a29c76b77b160e38aed
marceloandrade1993 <marcelowow13@hotmail.com>
2024-09-07 03:19:31 -0300

    Update dados.js
    projeto final.

--- dados.js
+++ dados.js
@@ -1,10 +1,32 @@
-let dados = [
-    {
-        titulo : "Video1",
-        descriçao : "treino de perna"
-    },
-    {
-        titulo : "Video2",
-        descriçao : "treino superior"
-    }
+let dados = [ {
+    
+    titulo : "treino Inferiores perna coxa",
+    descriçao : "Agachemtno: fazer 3 series de 15 reptiçoes",
+    videoUrl: "img/videoagachar.mp4",
+    
+    
+},
+
+{
+    titulo : "treino Superiores peito ombro",
+    descriçao : "flexao: fazer 3 séries de 15 repetições",
+    videoUrl : "img/videopeito.mp4"
+   
+},
+
+{
+    titulo : "treino Costas lombar ",
+    descriçao : "Costa : fazer 3 series de 30 seg",
+    videoUrl : "img/videocosta.mp4"
+},
+
+{
+    titulo : "treino Braço biceps triceps antebraço",
+    descriçao : "antebraço : fazer 3 series de 10 repetições",
+    videoUrl : "img/videobraço.mp4"
+    
+}
+       
 ];
+
+
